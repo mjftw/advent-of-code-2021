bb9529151b784c0663f02cf5caf9260cf660d833
Merlin Webster <mjftwebster@gmail.com>
2021-12-02 08:33:13 +0000

    Solve day1 part 2

--- src/exercises/day1.rs
+++ src/exercises/day1.rs
@@ -14,23 +14,44 @@ fn parse_input(raw_input: &str) -> Vec<u32> {
   }
   parsed
 }
+fn solve_part1(input: &Vec<u32>) -> u32 {
+  // Would have been easier to use a loop, but solved FP style for fun
+  let (_, increases): (u32, u32) =
+    input[1..]
+      .iter()
+      .fold((input[0], 0), |(last_depth, increases), depth| {
+        if *depth > last_depth {
+          (*depth, increases + 1)
+        } else {
+          (*depth, increases)
+        }
+      });
+
+  increases
+}
+
+fn solve_part2(input: &Vec<u32>) -> u32 {
+  let mut increases = 0;
+  for i in 3..input.len() {
+    let last_window_sum: u32 = input[(i - 3)..=(i - 1)].iter().sum();
+    let window_sum: u32 = input[i - 2..=i].iter().sum();
+
+    if window_sum > last_window_sum {
+      increases += 1;
+    }
+  }
+
+  increases
+}
 
 impl Exercise for Day1 {
   fn solve(&self, raw_input: &str) -> String {
     let input = parse_input(raw_input);
 
-    // Would have been easier to use a loop, but solved FP style for fun
-    let (_, increases): (u32, u32) =
-      input[1..]
-        .iter()
-        .fold((input[0], 0), |(last_depth, increases), depth| {
-          if *depth > last_depth {
-            (*depth, increases + 1)
-          } else {
-            (*depth, increases)
-          }
-        });
-
-    format!("{}", increases)
+    format!(
+      "Part1 = {}, Part2 = {}",
+      solve_part1(&input),
+      solve_part2(&input)
+    )
   }
 }
